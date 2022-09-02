28c7f800daa69c16a13fd9e988d57862f5d36a5e
CHMing <654849388@qq.com>
2022-09-02 15:26:58 +0800

    perf:解决build.gradle.kts报错问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    idea apply true
-    id("java")
     kotlin("jvm") version "1.5.31"
-    id("org.jetbrains.intellij") version "1.7.0"
-    id("org.jetbrains.changelog") version "1.3.1"
-    id("com.github.ben-manes.versions") version "0.42.0"
+    id("org.jetbrains.intellij") version "0.7.3"
 }
 
 group = "com.chm.plugin.idea.forestx"
@@ -18,16 +14,18 @@ repositories {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2020.2.4")
-    type.set("IU") // 企业版
-    plugins.set(listOf("java", "Spring", "SpringBoot", "yaml"))
+    version = "2020.2.4"
+    type = "IU" // 企业版
 
-    pluginName.set("ForestX")
-    sandboxDir.set("${rootProject.rootDir}/idea-sandbox")
+    setPlugins("java", "Spring", "SpringBoot", "yaml")
 
-    updateSinceUntilBuild.set(false)
-    downloadSources.set(true)
+    pluginName = "ForestX"
+    sandboxDirectory = "${rootProject.rootDir}/idea-sandbox"
+
+    updateSinceUntilBuild = false
+    downloadSources = true
 }
+
 
 dependencies {
     testImplementation(kotlin("test"))
@@ -38,5 +36,5 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "1.8"
 }
